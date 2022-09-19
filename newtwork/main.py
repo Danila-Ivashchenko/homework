20f45b54e423dd7515dbef6d8bd67d012c43e350
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2022-09-19 17:32:47 +0800

    починили интернет

--- newtwork/main.py
+++ newtwork/main.py
@@ -1,31 +1,5 @@
-input = open('input.txt', 'r')
-output = open('output.txt', 'w')
-
-array = []
-array_pairs = []
-array_islands = []
-
-def arr_to_str(arr):
-	print(arr)
-	str = ""
-	for item in arr:
-		for i in item:
-			str += f"{i}, "
-	return str[:-1]
-
-def find_pair(pair, arr):
-	for item in arr:
-		if pair == item:
-			return True
-	return False
-
-def find_island(island, arr):
-	for item in island:
-		for island_checkd in arr:
-			if item in island_checkd:
-				return True
-	return False
-
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def clean_str(str):
 	if str[-1] == '\n':
@@ -33,54 +7,51 @@ def clean_str(str):
 	else:
 		return str
 
-str = input.readline()
-n = int(str.split(' ')[-1])
+input = open('input_unnown.txt', 'r')
 
-array_of_points = [0 for i in range(n)]
+n = int(input.readline().split(' ')[-1])
+arr = [0] * n
+arr_pairs = []
 
-str = input.readline()
+count_isle = 1
+islands = 0
 
-while (str != ""):
-	first, second = int(clean_str(str).split(' ')[0]), int(clean_str(str).split(' ')[1])
-	if not(first in array):
-		array += [first]
-	if not(second in array):
-		array += [second]
-	array_pairs += [[first, second]]
-	str = input.readline()
+for line in input.readlines():
+	first, second = int(clean_str(line).split(' ')[0]), int(clean_str(line).split(' ')[1])
+	arr_pairs += [(first, second)]
+	if arr[first - 1] == arr[second - 1] == 0:
+		arr[first - 1] = arr[second - 1] = count_isle
+		count_isle += 1
+		islands += 1
+	elif arr[first - 1] != 0 and arr[second - 1] != 0:
+		for i in range(n):
+			if arr[i] == arr[second - 1]:
+				arr[i] = arr[first - 1]
+		islands -= 1
+	elif arr[first - 1] != 0 or arr[second - 1] != 0:
+		if arr[first - 1] != 0:
+			arr[second - 1] = arr[first - 1]
+		elif arr[second - 1] != 0:
+			arr[first - 1] = arr[second - 1]
+	count_isle += 1
 
-print(array_pairs)
-
-count_islands = 0
-
-for i in range(len(array_pairs)):
-	buf = [array_pairs[i]]
-	for j in range(len(array_pairs)):
-		if i != j:
-			if (array_pairs[i][0] in array_pairs[j] or array_pairs[i][1] in array_pairs[j]) and not(find_pair(array_pairs[i], array_islands) and find_pair(array_pairs[j], array_islands)):
-				buf += [array_pairs[j]]
-	if len(buf) > 0 and not(find_island(buf, array_islands)):
-		count_islands += 1
-		for pair in buf:
-			for point in pair:
-				array_of_points[point - 1] = count_islands
-		array_islands += [buf]
-
-
-for island in array_islands:
-	output.write(arr_to_str(island) + '\n')
-
-print(array_of_points)
-for i in range(n):
-	if array_of_points[i] == 0:
-		count_islands += 1
-		array_of_points[i] = count_islands
-
-print(array_of_points)
-print("Необходимо добавить: ", count_islands - 1, " связей")
-
-output.close()
 input.close()
+
+graph = nx.Graph()
+
+for pair in arr_pairs:
+	graph.add_edge(pair[0], pair[1])
+
+for i in range(n):
+	if arr[i] == 0:
+		arr[i] = count_isle
+		graph.add_node(str(i + 1))
+		count_isle += 1
+		islands += 1
+
+print(islands - 1)
+nx.draw(graph, with_labels = True)
+plt.show()
 
 
 
